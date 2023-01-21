5660c9033e3434fc121f9e148eb08d884ec7a88a
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2023-01-21 16:07:45 +0000

    ...

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" data-bs-theme="dark">
     
     <!-- Logo (Text for now) -->
-    <a class="navbar-brand" style="margin-left:7px;" href="index.php">GibJohn Tutoring</a>
+    <a class="navbar-brand" style="margin-left:15px;" href="index.php">Gibjohn</a>
     
     <!-- Phone Level of Display -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,12 +26,12 @@
     <?php       
         if (!isset($_SESSION['loggedin'])) {
             echo '<div class="navbar-nav">
-                    <form action="login.php"><button class="btn btn-primary" style="margin-right:7px" type="submit">Login</button></form>
+                    <form action="login.php"><button class="btn btn-primary" style="margin-right:4px" type="submit">Login</button></form>
                     <form action="register.php"><button class="btn btn-primary" style="margin-right:7px" type="submit">Register</button></form>
                 </div>';
         } elseif (loginCheck()===1) {
             echo '<div class="navbar-nav">
-                    <form action="dashboard.php"><button class="btn btn-primary" style="margin-right:7px" type="submit">Dashboard</button></form>
+                    <form action="dashboard.php"><button class="btn btn-primary" style="margin-right:4px" type="submit">Dashboard</button></form>
                     <form action="scripts/logoutScript.php"><button class="btn-danger btn" style="margin-right:7px" type="submit">Logout</button></form>
                 </div>';
         };
