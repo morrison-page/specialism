e4d58ef0a90d4064f5817c030eef2b4373d7f319
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2023-01-22 18:15:08 +0000

    ...

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
-<link rel="shortcut icon" href="gibjohn.ico" type="image/x-icon">
+<link rel="shortcut icon" href="misc/gibjohn.ico" type="image/x-icon">
 <?php require 'scripts/utils.php'; ?>
 <?php require 'includes/sessionStart.php'; ?>
 <title>Gibjohn Tutoring</title>
